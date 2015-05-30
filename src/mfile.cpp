9493e718ddccc3b790a4bfd807a99153defd91ca
Lyubomir <poseidon4o.random@gmail.com>
2015-05-30 16:44:22 +0100

    Reorganize in mpage & mfile

--- src/mfile.cpp
+++ src/mfile.cpp
@@ -2,66 +2,6 @@
 #include <algorithm>
 
 
-PageItemProxy & PageItemProxy::operator=(const uint8_t & value)
-{
-    MemoryPage & page = reinterpret_cast<MemoryPage&>(*this);
-    page.m_Data[page.m_ProxyIndex] = value;
-    page.m_Dirty = true;
-    return *this;
-}
-
-PageItemProxy::operator uint8_t()
-{
-    MemoryPage & page = reinterpret_cast<MemoryPage&>(*this);
-    return page.m_Data[page.m_ProxyIndex];
-}
-
-
-MemoryPage::MemoryPage() :
-    m_Data(nullptr), m_Size(0), m_Start(0), m_Dirty(false)
-{
-}
-
-int MemoryPage::size() const
-{
-    return m_Size;
-}
-
-uint64_t MemoryPage::start() const
-{
-    return m_Start;
-}
-
-bool MemoryPage::dirty() const
-{
-    return m_Dirty;
-}
-
-void MemoryPage::reset(int start, int size)
-{
-    m_Start = start;
-    m_Size = size;
-    m_Dirty = false;
-}
-
-uint8_t *& MemoryPage::data()
-{
-    return m_Data;
-}
-
-PageItemProxy & MemoryPage::relative(uint64_t idx)
-{
-    m_ProxyIndex = idx;
-    return reinterpret_cast<PageItemProxy&>(*this);
-}
-
-PageItemProxy & MemoryPage::absolute(uint64_t idx)
-{
-    return relative(idx - m_Start);
-}
-
-
-
 MemoryMapped::MemoryMapped(const std::string fileName) :
     m_FileName(std::move(fileName)), m_File(nullptr), m_UsedPages(0)
 {
---
+++ src/mpage.h
@@ -0,0 +1,50 @@
+#ifndef _MPAGE_H_INCLUDED_
+#define _MPAGE_H_INCLUDED_
+
+#include <cstdint>
+
+class PageItemProxy {
+public:
+    PageItemProxy() = delete;
+    PageItemProxy(const PageItemProxy &) = delete;
+    PageItemProxy & operator=(const PageItemProxy &) = delete;
+    PageItemProxy * operator&() = delete;
+
+    PageItemProxy & operator=(const uint8_t & value);
+    operator uint8_t();
+};
+
+
+class MemoryPage {
+    friend class MemoryMapped;
+    friend class PageItemProxy;
+
+    PageItemProxy & relative(uint64_t idx);
+    PageItemProxy & absolute(uint64_t idx);
+
+    int size() const;
+    bool dirty() const;
+    uint64_t start() const;
+
+    void reset(int start, int size);
+
+    // Unsafe - does not update dirty flag
+    uint8_t *& data();
+
+    MemoryPage();
+
+    MemoryPage(const MemoryPage &) = delete;
+    MemoryPage & operator=(const MemoryPage &) = delete;
+
+private:
+    uint8_t * m_Data;
+    int m_Size, m_Start;
+
+    // Since this is single threaded use only we can store the last access index
+    // and the proxy can be zero-sized
+    int m_ProxyIndex;
+
+    bool m_Dirty;
+};
+
+#endif // _MPAGE_H_INCLUDED_
