810ea358d20c282f6f0481027d7dfc000f7023b2
Lyubomir <poseidon4o.random@gmail.com>
2015-05-31 22:21:53 +0100

    Fix tests

--- src/main.cpp
+++ src/main.cpp
@@ -115,34 +115,41 @@ END_TEST
 
 
 START_TEST(sequential_move)
-// this clever move may look random but utilizes the pagin to it's best potential
+// seems random but utilizes the paging to it's best potential
 for (int c = 0; c < MemoryMapped::pageSize * 10; ++c) {
     for (int r = 0; r < MemoryMapped::pageCount / 2; ++r) {
-        file[r * MemoryMapped::pageSize + c] = file[r * MemoryMapped::pageSize + c + (MemoryMapped::pageCount / 2 + 1) * (MemoryMapped::pageSize * 10)];
+        file[r * MemoryMapped::pageSize + c] = (uint8_t&)file[r * MemoryMapped::pageSize + c + (MemoryMapped::pageCount / 2 + 1) * (MemoryMapped::pageSize * 10)];
     }
+}
+END_TEST
+
+START_TEST(breaking_move)
+// worst case for fixed position paging
+for (int c = MemoryMapped::pageSize; c < fileSize; c += MemoryMapped::pageSize) {
+    file[c] = (uint8_t&)file[c - 1];
 }
 END_TEST
 
 START_TEST(random_move)
 uniform_int_distribution<uint64_t> dist(0, fileSize - 1);
 for (int c = 0; c < 100000; ++c) {
-    file[dist(rnd)] = file[dist(rnd)];
+    file[dist(rnd)] = (uint8_t&)file[dist(rnd)];
 }
 END_TEST
 
 
-int main() {
+int main(int argc, char * argv[]) {
     try {
-        test_forward_read();
-        test_backward_read();
-
-        //test_stride_forward_read();
-        //test_stride_backward_read();
-        //
+        //test_forward_read();
         //test_forward_write();
+
+        //test_backward_read();
         //test_backward_write();
 
+        //test_stride_forward_read();
         //test_stride_forward_write();
+
+        //test_stride_backward_read();
         //test_stride_backward_write();
 
         //test_random_read_100000();
@@ -150,11 +157,22 @@ int main() {
 
         //test_sequential_move();
         //test_random_move();
+        //test_breaking_move();
+
+        if (argc != 3) {
+            std::cout << "wrong arguments";
+            return -1;
+        }
+        MemoryMapped mfile(argv[1]);
+        auto start = high_resolution_clock::now();
+        for (int c = 0; c < mfile.size(); ++c) {
+            mfile[c] = argv[2][0];
+        }
+        auto end = high_resolution_clock::now();
+        std::cout << duration_cast<milliseconds>(end - start).count();
     } catch (std::exception & e) {
         std::cerr << e.what() << std::endl;
     }
-
-    std::cout << "done";
 
     return 0;
 }
